0a2b830f9c0f5ad0d0c1a4367105e4ee3c3e6bbd
sarva1210 <sarva.vish77@gmail.com>
2026-04-07 18:47:42 +0530

    mail service

--- backend/mail.service.js
+++ backend/mail.service.js
@@ -12,7 +12,9 @@ const transporter = nodemailer.createTransport({
 })
 
 transporter.verify()
-    .then(() => { console.log("Email transporter is ready to send emails"); })
+    .then(() => { 
+        console.log("Email transporter is ready to send emails"); 
+    })
     .catch((err) => { console.error("Email transporter verification failed:", err); });
 
 export async function sendEmail({ to, subject, html, text = "" }) {
