079ea501b43179aa738db78b032cc008cfa5bce1
sarva1210 <sarva.vish77@gmail.com>
2026-04-07 18:48:33 +0530

    integrating mail

--- backend/mail.service.js
+++ backend/mail.service.js
@@ -15,7 +15,9 @@ transporter.verify()
     .then(() => { 
         console.log("Email transporter is ready to send emails"); 
     })
-    .catch((err) => { console.error("Email transporter verification failed:", err); });
+    .catch((err) => { 
+        console.error("Email transporter verification failed:", err); 
+    });
 
 export async function sendEmail({ to, subject, html, text = "" }) {
 
